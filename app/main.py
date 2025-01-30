cfee7b7cd4f7b8daff94d347707c524b0d345bca
Kirill Gubkin <k.gubkin@list.ru>
2025-01-30 10:09:37 +0600

    add class HotelsSearchArgs

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query, Depends
+from typing import Optional 
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def hello():
-    return 'hello'
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5)
+    ):
+        self.location= location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+@app.get('/hotels')
+def get_hotels(
+    search_args: HotelsSearchArgs = Depends()
+):
+   
+    return date_from, date_to
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
