997ce7d547c5b7e02ed5e64c60ad3e0bedfc1e75
Kirill Gubkin <k.gubkin@list.ru>
2025-02-04 15:22:27 +0600

    add register and login user

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from app.users.auth import get_password_hash
+from fastapi import APIRouter, HTTPException, Response, status
+from app.users.auth import authenticate_user, get_password_hash, verify_password
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserAuth
+from app.users.auth import create_access_token
 
 router = APIRouter(
     prefix='/auth',
@@ -9,10 +10,21 @@ router = APIRouter(
 )
 
 @router.post('/register')
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     exist_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if exist_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post('/login')
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    acces_token = create_access_token({'sub': user.id})
+    response.set_cookie('booking_access_token', acces_token, httponly=True)
+    return acces_token
     
